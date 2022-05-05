1e7638851b651c9ce1a01965672fe128d61c18e6
Luke Coles <12luke04@gmail.com>
2022-05-05 16:06:37 +0100

    Submit commit

---
+++ Sprites.h
@@ -0,0 +1,12 @@
+#ifndef SPRITES_H
+#define SPRITES_H
+
+//Header file to declare external sprite variables: https://stackoverflow.com/questions/12290451/access-extern-variable-in-c-from-another-file
+extern int pauseBig_s[37][42];
+extern int playBig_s[37][42];
+extern int pauseSmall_s[8][8];
+extern int playSmall_s[8][8];
+extern int noteSmall_s[8][8];
+extern int gearSmall_s[8][8];
+
+#endif //SPRITES_H
